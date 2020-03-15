1e189e67097cd6848837466f330d1307116e9e85
Sourav Singh Rawat <souravrawat1999@gmail.com>
2020-03-16 01:28:46 +0530

    Add files via upload

---
+++ src/main/java/in/irotech/animation/MainActivity.java
@@ -0,0 +1,24 @@
+package in.irotech.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout constraintLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        constraintLayout=findViewById(R.id.layout);
+        AnimationDrawable animationDrawable= (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
+    }
+}
